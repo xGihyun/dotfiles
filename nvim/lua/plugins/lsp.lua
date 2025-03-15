cc101d334fa72969c3a7cbea0a3a28d68d9ac83a
xGihyun <giordnnuz@gmail.com>
2025-03-15 15:45:29 +0800

    Added function for `root_dir` on `tailwindcss` LSP to only start on projects with Tailwind CSS installed

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -18,9 +18,28 @@ return {
         lua_ls = {},
         rust_analyzer = {},
         svelte = {},
-        -- tailwindcss = {
-        --   filetypes_exclude = { "markdown" },
-        -- },
+        tailwindcss = {
+          root_dir = function(fname)
+            local package_json = vim.fs.dirname(vim.fs.find("package.json", { path = fname, upward = true })[1])
+            if not package_json then
+              return nil
+            end
+
+            local file = io.open(package_json .. "/package.json", "r")
+            if not file then
+              return nil
+            end
+
+            local content = file:read("*a")
+            file:close()
+
+            if content:match('"tailwindcss"%s*:') then
+              return package_json
+            else
+              return nil
+            end
+          end,
+        },
         eslint = {},
         html = {},
         cssls = {},
