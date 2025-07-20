78a9009bb081e95cfa1f2f6a26b3805ee488961d
xGihyun <giordnnuz@gmail.com>
2025-07-20 10:24:23 +0800

    remove unused `lazy-vim` config

--- lazy-nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.set("n", "<tab>", "<cmd>bnext<cr>")
-vim.keymap.set("n", "<S-tab>", "<cmd>bprevious<cr>")
-vim.keymap.set("i", "jj", "<Esc>")
--- lazy-nvim/lua/plugins/write.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "preservim/vim-pencil",
-  lazy = true,
-  event = {
-    "BufReadPre " .. vim.fn.expand("~") .. "/Documents/Obsidian Vault/**.md",
-    "BufNewFile " .. vim.fn.expand("~") .. "/Documents/Obsidian Vault/**.md",
-  },
-}
