553595b11e6d8ebe18a89753ba5742873af5b459
xGihyun <giordnnuz@gmail.com>
2025-07-21 14:42:00 +0800

    scripts(notify): randomly select notification sound

--- _scripts/notify.sh
+++ _scripts/notify.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-ffplay -nodisp -volume 25 -autoexit ~/.config/_audio/panpakapan.mp3 & disown
+dir="$HOME/.config/_audio/"
+
+file=$(find "$dir" -type f -iname "*.wav" -print0 | shuf -zn1 | tr -d '\0')
+if [[ -n $file ]]; then
+    ffplay -nodisp -volume 25 -autoexit "$file" & disown
+else
+    echo "No WAV files found in $dir." >&2
+    exit 1
+fi
