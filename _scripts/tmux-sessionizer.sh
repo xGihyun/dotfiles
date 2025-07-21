f6e208b108090d2945fe6d3702e64cab11e89cff
xGihyun <giordnnuz@gmail.com>
2025-07-21 08:14:00 +0800

    scripts: update `tmux-sessionizer` to fuzzy find both directories and existing sessions

--- _scripts/tmux-sessionizer.sh
+++ _scripts/tmux-sessionizer.sh
@@ -2,30 +2,39 @@
 
 search_paths=(~/ ~/Documents)
 
-if [[ $# -eq 1 ]]; then
-    selected=$1
+display_items=()
+data_items=()
+
+while IFS= read -r session; do
+    [[ -n $session ]] && display_items+=("󰈺  $session") && data_items+=("session:$session")
+done < <(tmux list-sessions -F "#{session_name}" 2>/dev/null)
+
+while IFS= read -r dir; do
+    [[ -n $dir ]] && display_items+=("󰉋  $dir") && data_items+=("directory:$dir")
+done < <(find "${search_paths[@]}" -mindepth 1 -maxdepth 1 -type d)
+
+selection=$(printf '%s\n' "${display_items[@]}" | fzf)
+[[ -z $selection ]] && exit 0
+
+for i in "${!display_items[@]}"; do
+    if [[ "${display_items[i]}" == $selection ]]; then
+        selected_data="${data_items[i]}"
+        break
+    fi
+done
+
+type=${selected_data%%:*}
+name=${selected_data#*:}
+
+if [[ $type == "session" ]]; then
+    [[ -z $TMUX ]] && tmux attach -t $name || tmux switch-client -t $name
 else
-    selected=$(find "${search_paths[@]}" -mindepth 1 -maxdepth 1 -type d | fzf)
-fi
-
-if [[ -z $selected ]]; then
-    exit 0
-fi
-
-selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
-
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
-    exit 0
-fi
-
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
-fi
-
-if [[ -z $TMUX ]]; then
-    tmux attach -t $selected_name
-else
-    tmux switch-client -t $selected_name
+    session_name=$(echo $name | tr . _)
+    
+    if [[ -z $TMUX ]]; then
+        tmux new-session -s $session_name -c $dir
+    else
+        tmux new-session -ds $session_name -c $dir
+        tmux switch-client -t $session_name
+    fi
 fi
