0d4842c549656645ea00c9e0ce881d76a7080eec
xGihyun <giordnnuz@gmail.com>
2025-07-30 11:11:31 +0800

    scripts(power-menu): remove suspend and hibernate

--- rofi/scripts/power-menu.sh
+++ rofi/scripts/power-menu.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-chosen=$(printf "󰐥  Power Off\n  Restart\n  Suspend\n  Hibernate\n󰍃  Log Out\n  Lock" | rofi -dmenu -i -p "Power Menu")
+chosen=$(printf "󰐥  Power Off\n  Restart\n󰍃  Log Out\n  Lock" | rofi -dmenu -i -p "Power Menu")
 
 case "$chosen" in
 "󰐥  Power Off") tmux kill-server & poweroff ;;
 "  Restart") tmux kill-server & reboot ;;
-"  Suspend") systemctl suspend-then-hibernate ;;
-"  Hibernate") systemctl hibernate ;;
 "󰍃  Log Out") hyprctl dispatch exit ;;
 "  Lock") betterlockscreen -l ;;
 *) exit 1 ;;
