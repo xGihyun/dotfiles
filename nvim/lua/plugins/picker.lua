08933d9b824190037c7cc287ca68aa984e4cb081
xGihyun <giordnnuz@gmail.com>
2025-08-09 07:42:38 +0800

    nvim(fzf-lua): add keybind for searching files with `no_ignore = true`

--- nvim/lua/plugins/picker.lua
+++ nvim/lua/plugins/picker.lua
@@ -4,6 +4,9 @@ return {
     local fzf = require("fzf-lua")
 
     vim.keymap.set("n", "<leader>ff", fzf.files, {})
+    vim.keymap.set("n", "<leader>fF", function()
+      fzf.files({ no_ignore = true })
+    end, {})
     vim.keymap.set("n", "<leader>fg", fzf.live_grep, {})
     vim.keymap.set("n", "<leader>fb", fzf.buffers, {})
     vim.keymap.set("n", "<leader>ft", "<Cmd>TodoFzfLua<CR>", {
