4950eac64445ce9cea8503027122b1cfc982465c
xGihyun <giordnnuz@gmail.com>
2025-08-09 10:16:11 +0800

    scripts: run `gammastep` on startup

--- _scripts/startup.sh
+++ _scripts/startup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-swaybg -i ~/.config/_wallpapers/kayoko_dress-pixiv-116416657_p0.jpg -m fill &
+swaybg -i ~/.config/_wallpapers/kayoko_ny-pixiv-106760656_p0.jpg -m fill &
 waybar &
 xwayland-satellite &
 mako &
+~/.config/_scripts/gammastep-toggle.sh &
 
 disown -a 
