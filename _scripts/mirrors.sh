1dabfba3590d66f8367d73e2318efe5709ab4875
xGihyun <grdnnz.dev@proton.me>
2025-09-03 07:20:36 +0800

    scripts: update options passed to `mirrors`

--- _scripts/mirrors.sh
+++ _scripts/mirrors.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-reflector --verbose -c HK -c ID -c SG -c TW -c TH -c VN --protocol https --sort rate --latest 20 --download-timeout 5 --threads 4 > /etc/pacman.d/mirrorlist
+reflector -c HK,ID,SG,TW,TH,VN \
+    --protocol https \
+    --sort score \
+    --latest 40 \
+    --fastest 20 \
+    --age 12 \
+    --download-timeout 5 \
+    --threads 4 \
+    --save /etc/pacman.d/mirrorlist
