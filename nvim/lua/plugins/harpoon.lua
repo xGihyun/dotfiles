3794975aeb1efb8ef9cabeb2c6cef80b37ba751b
xGihyun <grdnnz.dev@proton.me>
2025-09-21 07:50:42 +0800

    nvim: add keymap description for harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -16,10 +16,10 @@ return {
     end)
     vim.keymap.set("n", "<leader>h", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    end, { desc = "[H]arpoon" })
 
     -- Set <space>1..<space>5 be my shortcuts to moving to the files
-    for _, idx in ipairs { 1, 2, 3, 4, 5 } do
+    for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
       vim.keymap.set("n", string.format("<space>%d", idx), function()
         harpoon:list():select(idx)
       end)
