f7351d455220a1417854e5ce775bbd74922d5306
xGihyun <grdnnz.dev@proton.me>
2025-10-05 09:09:00 +0800

    feat: make `mirrors` script more responsive

--- _scripts/mirrors.sh
+++ _scripts/mirrors.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+backup_dir=/etc/pacman.d/mirrorlist.bak
+
+set -e
+
+echo "Starting mirrorlist update.\n"
+
+echo "Backing up current mirrorlist..."
+cp /etc/pacman.d/mirrorlist $backup_dir
+echo "Backup created at $backup_dir\n"
+
+echo "Running reflector to find mirrors..."
+echo
+
 reflector -c HK,ID,SG,TW,TH,VN \
     --protocol https \
     --sort score \
@@ -9,4 +21,8 @@ reflector -c HK,ID,SG,TW,TH,VN \
     --age 12 \
     --download-timeout 5 \
     --threads 4 \
+    --verbose \
     --save /etc/pacman.d/mirrorlist
+
+echo
+echo "Done."
